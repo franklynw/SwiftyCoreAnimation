b00607479a3f5e21d7e7037f4b710f6f32affcba
Franklyn Weber <franklynweber@Franklyns-MacBook-Pro.local>
2019-04-04 09:54:22 +0100

    Added missing sub-properties to VisibleRect

--- CoreAnimation/Layer Properties/CALayer/Unanimatable/VisibleRect.swift
+++ CoreAnimation/Layer Properties/CALayer/Unanimatable/VisibleRect.swift
@@ -15,4 +15,66 @@ public struct VisibleRect: UnsettableLayerPropertyConformance {
 
     public let value: KeyValueType?
     public static var keyPath: String { return "visibleRect" }
+
+    public struct Origin: UnsettableLayerPropertyConformance {
+        public typealias KeyValueType = CGPoint
+
+        public let value: KeyValueType?
+        public static var keyPath: String { return VisibleRect.keyPath.appendingKeyPath("origin") }
+
+        public struct X: UnsettableLayerPropertyConformance {
+            public typealias KeyValueType = CGFloat
+
+            public let value: KeyValueType?
+            public static var keyPath: String { return Origin.keyPath.appendingKeyPath("x") }
+
+            public init(_ value: KeyValueType) {
+                self.value = value
+            }
+        }
+
+        public struct Y: UnsettableLayerPropertyConformance {
+            public typealias KeyValueType = CGFloat
+
+            public let value: KeyValueType?
+            public static var keyPath: String { return Origin.keyPath.appendingKeyPath("y") }
+
+            public init(_ value: KeyValueType) {
+                self.value = value
+            }
+        }
+    }
+
+    public struct Size: UnsettableLayerPropertyConformance {
+        public typealias KeyValueType = CGSize
+
+        public let value: KeyValueType?
+        public static var keyPath: String { return VisibleRect.keyPath.appendingKeyPath("size") }
+
+        public init(_ value: KeyValueType) {
+            self.value = value
+        }
+
+        public struct Width: UnsettableLayerPropertyConformance {
+            public typealias KeyValueType = CGFloat
+
+            public let value: KeyValueType?
+            public static var keyPath: String { return Size.keyPath.appendingKeyPath("width") }
+
+            public init(_ value: KeyValueType) {
+                self.value = value
+            }
+        }
+
+        public struct Height: UnsettableLayerPropertyConformance {
+            public typealias KeyValueType = CGFloat
+
+            public let value: KeyValueType?
+            public static var keyPath: String { return Size.keyPath.appendingKeyPath("height") }
+
+            public init(_ value: KeyValueType) {
+                self.value = value
+            }
+        }
+    }
 }
